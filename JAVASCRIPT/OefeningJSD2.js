6b11bc5cb466d49363f3f94c3d2fb8962cb971c4
Andy123567 <andy.vandeweyer08@gmail.com>
2021-03-04 12:26:13 +0100

    Standaard boekhandel

--- JAVASCRIPT/OefeningJSD2.js
+++ JAVASCRIPT/OefeningJSD2.js
@@ -1,3 +1,4 @@
+// Oefening 1
 let Zin1, Zin2, Zin3;
 
 Zin1= "Dit is de eerste zin"
@@ -12,6 +13,7 @@ console.log(Zin1);
 console.log(Zin2);
 console.log(Zin3);
 
+// Oefening 2
 let array = ('Dit is zin 1', 'Dit is zin 2', 'Dit is zin 3');
 let output;
 
@@ -19,3 +21,13 @@ array.forEach(zin => { output += ' ' + zin });
 alert(Zin1 + ' ' + Zin2 + ' ' + Zin3);
 alert(`${Zin1} dit is een doorlopende tekst ${Zin2} en ${Zin3}`);
 alert(output);
+
+// Oefening 3
+let koekje = 'Ik wil een koekje';
+console.log(koekje.charAt(0));
+console.log(koekje.charAt(3));
+console.log(koekje.lastIndexOf('k'));
+console.log(koekje.lastIndexOf('e'));
+console.log(koekje.length);
+
+
