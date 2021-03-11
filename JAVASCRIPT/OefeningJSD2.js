469ee5c7654e4850cd3dae3179881428eff2b793
Andy123567 <andy.vandeweyer08@gmail.com>
2021-03-11 21:20:45 +0100

    Update OefeningJSD2.js
    Boe

--- JAVASCRIPT/OefeningJSD2.js
+++ JAVASCRIPT/OefeningJSD2.js
@@ -31,4 +31,26 @@ console.log(koekje.lastIndexOf('k'));
 console.log(koekje.lastIndexOf('e'));
 console.log(koekje.length);
 
+let lasagne = koekje.replace( 'koekje',  'lasagne van de pxl');
+document.getElementById( 'lasagne').innerHTML = lasagne.toUpperCase();
 
+let woord = prompt ('geef een woord in');
+let lengte = woord.length;
+alert(`De lengte van het woord ${woord} is ${lengte}`);
+
+let zin = prompt('Geef een zin in');
+let indexOfSpace = zin.indexOf(' ');
+console.log('De eerste spatie staat op positie ' + indexOfSpace);
+
+let getal1 = prompt('Geef een getal in');
+let getal2 = prompt('Geef een tweede getal in');
+let vermenigvuldiging = getal1 * getal2;
+let opstellen = parseFloat(getal1)  + parseFloat(getal2);
+
+let uitkomst = 'De uitkomst van de vermenigvuldiging is: ' + vermenigvuldiging;
+document.getElementById('uitkomst').innerText = uitkomst;
+
+let oudgenoeg = prompt('Bent je ouder dan 18?');
+if (!oudgenoeg === 'ja'){
+    alert('sorry, probeer het volgend jaar opnieuw')
+}
